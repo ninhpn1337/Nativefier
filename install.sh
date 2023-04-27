d0e18fc8bd641bb71d267fdd9d8d8807eb80e209
ninh.pn1337 <ninh.pn1337@gmail.com>
2023-04-27 00:31:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,7 @@
-#!/bin/sh
-set -e
-WORKING_DIR=`pwd`
-THIS_PATH=`readlink -f $0`
-cd `dirname ${THIS_PATH}`
-FULL_PATH=`pwd`/Lotion
-cd ${WORKING_DIR}
-cat <<EOS > Messenger.desktop
-[Desktop Entry]
-Name=Messenger
-Name[en_US]=Messenger
-Comment=Unofficial Facebook Messenger application for Linux
-Exec="${FULL_PATH}/Messenger"
-Terminal=false
-Categories=Social
-Type=Application
-Icon=${WORKING_DIR}/icon.png
-StartupWMClass=Messenger
-EOS
-chmod +x Messenger.desktop
-## This can be updated if this path is not valid. 
-cp -p Messenger.desktop ~/.local/share/applications
+#!/bin/bash
+PD=`pwd`
+INSTALL_DIR=$PD"/Messenger"
+echo $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+tar xvf $PD/Messenger-linux-x64.tar.xz -C $INSTALL_DIR --strip 1
+/bin/bash $PD/shortcut.sh
